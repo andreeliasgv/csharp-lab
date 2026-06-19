d20b2aa83238d21ec753c9bfe64f8340d7383805
andreeliasgv <andrevila2016@hotmail.com>
2026-06-18 23:51:02 -0300

    Criação do projeto

---
+++ calculator/Program.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /* Proposta da Atividade
+             * 
+             * 1) Calculadora simples
+             * Crie um programa que receba dois números e mostre:
+             * 
+             * soma
+             * subtração
+             * multiplicação
+             * divisão
+             * 
+             * Foco: tipos de dados, variáveis, conversão, operadores aritméticos.
+             * 
+             */
+
+            #region "Variables"
+
+            double numero1, numero2, resultado = 0.0;
+            int operacao;
+
+            #endregion
+
+            #region "Headline"
+
+            Console.WriteLine("=========================");
+            Console.WriteLine("== CALCULADORA SIMPLES ==");
+            Console.WriteLine("=========================");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionalidades: Adição, Subtração, Multiplicação e Divisão");
+
+            Console.WriteLine();
+
+            #endregion
+
+            Console.Write("Digite o primeiro número:\n> ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Selecione uma operação:");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            Console.WriteLine();
+
+            Console.Write("> ");
+
+            operacao = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Digite o segundo número:\n> ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+
+            switch (operacao)
+            {
+                case 1:
+                    resultado = numero1 + numero2;
+                    break;
+                case 2:
+                    resultado = numero1 - numero2;
+                    break;
+                case 3:
+                    resultado = numero1 * numero2;
+                    break;
+                case 4:
+                    resultado = numero1 / numero2;
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida. Por favor, selecione uma operação válida!");
+                    break;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"O resultado da operação é: {resultado}");
+        }
+    }
+}
