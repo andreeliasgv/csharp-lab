ca81b95d847ed7e9ea6125f2b752e1ef93613ee2
andreeliasgv <andrevila2016@hotmail.com>
2026-06-23 21:26:40 -0300

    Implementações realizadas conforme orientações do desafio (README.md)

--- 09-MultiplesChecker/Program.cs
+++ 09-MultiplesChecker/Program.cs
@@ -7,24 +7,37 @@ class minhaClasse
 	static void Main(string[] args)
 	{
 
-		int suavariavel = int.Parse(Console.ReadLine());
-		string[] n = Console.ReadLine().Split(' ');
+		int qtdnumeros = int.Parse(Console.ReadLine());
+		string[] numeros = Console.ReadLine().Split(' ');
 
 		int multiplo2 = 0;
 		int multiplo3 = 0;
 		int multiplo4 = 0;
 		int multiplo5 = 0;
-		//TODO: Complete os espaços em branco com uma possível solução para o desafio
-		for (int iContador = 0;                        )
+		// DONE: Complete os espaços em branco com uma possível solução para o desafio
+		for (int iContador = 0; iContador < numeros.Length; iContador++)
 		{
-			if ((int.Parse(n[iContador]) %  ) == 0) ;
-			if ((int.Parse(n[iContador]) %  ) == 0) ;
-			if ((int.Parse(n[iContador]) %  ) == 0) ;
-			if ((int.Parse(n[iContador]) %  ) == 0) ;
+			if ((int.Parse(numeros[iContador]) % 2) == 0)
+			{
+				multiplo2++;
+			}
+			if ((int.Parse(numeros[iContador]) % 3) == 0)
+			{
+				multiplo3++;
+			}
+			if ((int.Parse(numeros[iContador]) % 4) == 0)
+			{
+				multiplo4++;
+			}
+			if ((int.Parse(numeros[iContador]) % 5) == 0)
+			{
+				multiplo5++;
+			}
 		}
 
-		Console.WriteLine("{0} Multiplo(s) de 2", multiplo2);
-		Console.WriteLine("{0} Multiplo(s) de 3", multiplo3);
-		Console.WriteLine("{0} Multiplo(s) de 4", multiplo4);
-		Console.WriteLine("{0} Multiplo(s) de 5", multiplo5);
+		Console.WriteLine($"{multiplo2} Multiplo(s) de 2");
+		Console.WriteLine($"{multiplo3} Multiplo(s) de 3");
+		Console.WriteLine($"{multiplo4} Multiplo(s) de 4");
+		Console.WriteLine($"{multiplo5} Multiplo(s) de 5");
 	}
+}
